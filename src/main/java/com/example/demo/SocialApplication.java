2cac4222f51ee67cc23cf44a773171a654ed91e3
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2018-03-08 16:30:46 +0600

    Initial basic app with facebook authentication

--- src/main/java/com/example/demo/SocialApplication.java
+++ src/main/java/com/example/demo/SocialApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-public class SimpleApplication {
+@EnableOAuth2Sso
+public class SocialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleApplication.class, args);
+		SpringApplication.run(SocialApplication.class, args);
 	}
 }
